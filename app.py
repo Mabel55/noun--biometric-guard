73d0766ec0a67439956b753e3f2638e366d77946
Mabel <nasaadanna@gmail.com>
2025-12-27 07:15:17 -0800

    My personal biometric project

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import csv
+app = Flask(__name__)
+
+# FUNCTION: Find student by Fingerprint ID
+def get_student_by_id(search_id):
+    try:
+        with open('student.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['fingerprint_id'] == search_id:
+                    return row
+                
+    except FileNotFoundError:
+        return None
+    return None
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    student = None
+    error = None
+    
+    if request.method == 'POST':
+        f_id = request.form['fingerprint_id']
+        student = get_student_by_id(f_id)
+        
+        if not student:
+            error = "NO MATCH FOUND! possible impersonater."
+    return render_template('index.html', student=student, error=error)
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+        
